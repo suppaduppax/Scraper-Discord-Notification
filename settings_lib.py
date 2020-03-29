b580fc59ec8c2018a021ebfd3dbfd21cde0ef871
alvin <mr.alvinaquino@gmail.com>
2020-03-29 23:15:34 +0000

    creates settings.yaml file when it doesnt exist fixed bug with getting log_rotation_files from settings Closes #13

--- settings_lib.py
+++ settings_lib.py
@@ -1,3 +1,4 @@
+import os
 import yaml
 
 def _get_defaults():
@@ -8,10 +9,25 @@ def _get_defaults():
     s["log_rotation_files"] = 5
     return s
 
+def get_comments():
+    return """\
+# log_rotation_files: number of files to keep in log rotation
+# global_include: a global include that spans across all tasks
+# global_exclude: a global exclude that spans across all tasks
+# recent_ads: only display the most recent number of ads 
+#             to prevent a large notification dump
+"""
+
 def load(file):
     global _settings
 
     _settings = _get_defaults()
+
+    if not os.path.exists(file):
+        with open(file, "w") as stream:
+            stream.write(get_comments())
+            yaml.safe_dump(_settings, stream)
+
     with open(file, "r") as stream:
         _settings = yaml.safe_load(stream)
 
